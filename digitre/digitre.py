fa8892ea4e7fb5ce9536aca633d80d51d80cf2aa
luisvalesilva <luisvalesilva@gmail.com>
2016-12-14 17:27:13 -0500

    restructure folder tree

---
+++ digitre/digitre.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+    Digitre
+    ~~~~~~~
+    A simple application...
+    :copyright: (c) 2016 by Luis Vale Silva.
+    :license: MIT, see LICENSE for more details.
+"""
+
+__author__ = "Luis Vale Silva"
+__status__ = "Development"
+
+
+from flask import Flask, render_template, request, jsonify
+import lib.helpers as helpers
+
+app = Flask(__name__)
+
+
+@app.route('/_get_digit')
+def get_digit():
+    """Get digit drawn by user as base64 image and convert to numpy array"""
+    digit = request.args.get('digit', 0, type=str)
+    helpers.b64_str_to_np(digit)
+    return jsonify(result=digit)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ helpers.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+"""
+    Digitre
+    ~~~~~~~
+    A simple application...
+    :copyright: (c) 2016 by Luis Vale Silva.
+    :license: MIT, see LICENSE for more details.
+"""
+
+__author__ = "Luis Vale Silva"
+__status__ = "Development"
+
+import base64
+from io import BytesIO
+from PIL import Image
+import numpy as np
+
+def b64_str_to_np(base64_str):
+    """Get digit drawn by user as base64 image and convert to numpy array"""
+    _, base64_str = base64_str.split(',')
+    buf = BytesIO()
+    buf.write(base64.b64decode(base64_str))
+    pimg = Image.open(buf)
+    img = np.array(pimg)
+
+    print('Dimensions of numpy ndarray: ', img.shape)
+    return(img)
+
+
+
+
+
+
+
+
+def main():
+    """
+    Main function
+    """
+    # ...
+    
+
+
+if __name__ == '__main__':
+    main()
