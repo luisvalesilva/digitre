fdfb696c9f2d98408d30f61e17bc2f81f82d7d52
luisvalesilva <luisvalesilva@gmail.com>
2016-12-14 19:32:03 -0500

    Working on functions to preprocess digit image

--- digitre/helpers.py
+++ digitre/helpers.py
@@ -19,29 +19,56 @@ import numpy as np
 
 def b64_str_to_np(base64_str):
     """Get digit drawn by user as base64 image and convert to numpy array"""
-    _, base64_str = base64_str.split(',')
+
+    if "base64" in base64_str:
+        _, base64_str = base64_str.split(',')
+
     buf = BytesIO()
     buf.write(base64.b64decode(base64_str))
     pimg = Image.open(buf)
     img = np.array(pimg)
 
     print('Dimensions of numpy ndarray: ', img.shape)
-    return(img)
+
+    # Keep only 4th pixel value in 3rd dimension (first 3 are all zeros)
+    return img[:,:,3]
+
+def lowest_non_zero(x):
+    """Determine index of first non-zero value"""
+    return np.nonzero(x)[0][0]
 
 
-
-
-
-
-
-
-def main():
-    """
-    Main function
-    """
-    # ...
+def crop_and_resize(img_ndarray):
+    """Crop white space around digit and resize to 28x28"""
+    # Length of zero pixel values for rows and columns
+    # Across rows
+    first_row = np.nonzero(img_ndarray)[0].min()
+    last_row = np.nonzero(img_ndarray)[0].max()
     
+    # Across cols
+    first_col = np.nonzero(img_ndarray)[1].min()
+    last_col = np.nonzero(img_ndarray)[1].max()
+
+    # Crop by longest non-zero to make sure all is kept
+    first = min(first_row, first_col)
+    last = max(last_row, last_col)
+
+    print(last-first)
+    # Minimum size of 28x28
+    if last-first < 28:
+        if first + 28 < 200 :
+            last = first + 28
+        else:
+            first = last - 28
+
+    # Crop image
+    img_ndarray = img_ndarray[first:last, first:last]
+
+    return img_ndarray.thumbnail((28, 28), Image.ANTIALIAS)
 
 
-if __name__ == '__main__':
-    main()
+
+def flatten_img(img_ndarray):
+    """Flatten digit image as numpy ndarray to 1D"""
+
+    return img_ndarray.flatten()
