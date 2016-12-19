8b3bf445dcfd3be70f0526a884c4d14925287906
luisvalesilva <luisvalesilva@gmail.com>
2016-12-18 23:54:46 -0500

    Added padding around centered digits

--- digitre/prep_digit_image.py
+++ digitre/prep_digit_image.py
@@ -62,7 +62,9 @@ def crop_img(img_ndarray):
     last_col = middle_col + half_length
 
     # Crop image
-    return img_ndarray[first_row:last_row, first_col:last_col]
+    img_ndarray[first_row:last_row, first_col:last_col]
+    # Add padding (15px of zeros)
+    return np.lib.pad(img_ndarray, 15, 'constant', constant_values=(0))
 
 
 def resize_img(img_ndarray):
